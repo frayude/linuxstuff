a8dd271d956389fd2052398f4d1800a4823d5435
Fray <frayudaadriansyah@gmail.com>
2026-03-16 07:18:17 +0700

    add clipboard

--- hypr/scripts/clipboard.sh
+++ hypr/scripts/clipboard.sh
@@ -1,1 +1,10 @@
-cliphist list | rofi -dmenu | cliphist decode | wl-copy
+#!/bin/bash
+
+# Cek kalau rofi lagi jalan, kalau iya bunuh (toggle), kalau nggak lanjut jalanin
+if pgrep -x "rofi" > /dev/null; then
+    pkill -x "rofi"
+    exit 0
+fi
+
+# Jalankan clipboard dengan tema khusus agar beda dengan app launcher
+cliphist list | rofi -dmenu -theme ~/.config/rofi/clipboard.rasi | cliphist decode | wl-copy
