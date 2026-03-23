b02c47d3d1df46961484ff7ec63167633536a5d1
Fray <frayudaadriansyah@gmail.com>
2026-03-23 17:20:28 +0700

    add scripts capslock usd

---
+++ hypr/scripts/capslock.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STATE_FILE="/tmp/caps_lock_state"
+
+if [ ! -f "$STATE_FILE" ]; then
+    echo "0" > "$STATE_FILE"
+fi
+
+CURRENT_STATE=$(cat "$STATE_FILE")
+
+# Pake simbol panah macOS (󰬈) atau (⇪)
+if [ "$CURRENT_STATE" == "0" ]; then
+    notify-send -t 1200 -h string:x-dunst-stack-tag:caps "Caps Lock: ON"
+    echo "1" > "$STATE_FILE"
+else
+    notify-send -t 1200 -h string:x-dunst-stack-tag:caps "Caps Lock: OFF"
+    echo "0" > "$STATE_FILE"
+fi
+
